ffa5948a7b0599ce0c75ea9bcf35f618eca5766a
Austin <austin.x.diesta@kohls.com>
2023-12-20 17:42:33 -0600

    added to components and css

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,21 @@
+import { useState } from 'react';
+
 const Home = () => {
+
+	const [blogs, setBlogs] = useState([
+		{ title: 'Blog 1', body: 'lorem ipsum...', author: 'austin', id: 1},
+		{ title: 'Blog 2', body: 'lorem ipsum...', author: 'parker', id: 2},
+		{ title: 'Blog 3', body: 'lorem ipsum...', author: 'bennett', id: 3}
+	])
+
 	return (
 		<div className="home">
-			<h2>Homepage</h2>
+			{blogs.map((blog) => (
+				<div className="blog-preview" key={blog.id}>
+					<h2>{ blog.title }</h2>
+					<p>Written by { blog.author }</p>
+				</div>
+			))}
 		</div>
 	);
 }
